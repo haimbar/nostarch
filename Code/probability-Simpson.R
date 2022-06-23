555c65613caa6ae828c03f68871073c739441d40
Haim Bar <haim.bar@uconn.edu>
2022-06-23 23:42:39 +0300

    worked on Simpson's paradox

--- Code/probability-Simpson.R
+++ Code/probability-Simpson.R
@@ -3,10 +3,13 @@ g <- 4                            # number of groups
 n <- 40                           # number of instances in each group
 z <- rep(1:g, each = n)           # grouping variable
 x <- runif(n * g, 0, 2) + z       # x variable that depends on z
-y <- 2 * z - x + rnorm(n * g)     # y variable that depends on x and z
+y <- 3 * z - x + rnorm(n * g)     # y variable that depends on x and z
 pdf("images/probability-Simpson-plots.pdf", height=5, width=5)
-plot(x, y)                        # plot the whole data
+plot(x, y, pch=19, col=4, cex=0.7)# plot the whole data
+abline(lm(y ~ x), lwd=3, col="navyblue")   # show the overall linear trend
 plot(x, y, pch = z, col = z)      # plot the data with group labeled
+for (grp in 1:g)
+  abline(lm(y[z==grp] ~ x[z==grp]), lwd=3, col=grp, lty=2)
 dev.off()
 
 ## pdf("images/probability-Simpson-whole.pdf", height=5, width=5)
