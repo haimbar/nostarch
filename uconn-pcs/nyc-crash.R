9dd7f2db719860f5afc483855980a1f27351916b
Jun Yan <jun.yan@uconn.edu>
2022-07-12 13:51:17 -0400

    crash project

--- uconn-pcs/nyc-crash.R
+++ uconn-pcs/nyc-crash.R
@@ -5,13 +5,14 @@
 crash <- read.csv("https://github.com/statds/ids-s22/raw/main/notes/data/nyc_mv_collisions_202201.csv")
 
 ## what variables are in the dataset
-names(crash)
+names(crash) <- tolower(names(crash))
 
 ## frequency table by borough
-xtabs(~ BOROUGH, data = crash)
+xtabs(~ borough, data = crash)
 
 ## Create a hour variable with integer values from 0 to 23
-crash$hour <- as.numeric(sub(":[0-9][0-9]", "", crash$CRASH.TIME))
+crash$hour <- as.numeric(sub(":[0-9][0-9]", "", crash$crash.time))
 
 table(crash$hour)
 hist(crash$hour, breaks=24)
+
