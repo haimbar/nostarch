fdc0a4032cce03a4c26f5e7d4b4518fe6910799e
HaiYing Wang <whygps@gmail.com>
2023-03-27 22:01:31 -0400

    illustrate representative sample

---
+++ Code/datacollect-goodbadsample.R
@@ -0,0 +1,26 @@
+pdf("images/chapter_6/population.pdf", width=3.5, height=3.5)
+set.seed(2023)
+N = 500
+height <- rnorm(N, 70, 2.6)
+weight <- -194.49 + 5.30 * height + rnorm(N, 0, 23.26)
+population <- data.frame(height=height, weight=weight)
+plot(population)
+dev.off()
+
+pdf("images/chapter_6/representSample.pdf", width=3.5, height=3.5)
+plot(population)
+n <- 50
+idx <- sample(N, n)
+rsample <- population[idx,]
+points(rsample, col="green", pch=2, cex=1.2)
+dev.off()
+
+pdf("images/chapter_6/biasedSample.pdf", width=3.5, height=3.5)
+plot(population)
+n <- 50
+f = lm(weight ~ height)
+w = abs(f$residuals) / abs(height-70)
+idx <- sample(N, n, prob=w)
+rsample <- population[idx,]
+points(rsample, col="red", pch=2, cex=1.2)
+dev.off()
