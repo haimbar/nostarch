92a494c3f42276a8c24635770fa38e12c04795cd
Haim Bar <haim.bar@uconn.edu>
2023-05-18 19:30:06 -0400

    editted up to the Overlayed histograms section

--- Code/ch2-01gendat.R
+++ Code/ch2-01gendat.R
@@ -1,9 +1,9 @@
 set.seed(220526)
 x <- seq(0, 4 * pi, by=0.02)
-g <- factor(rbinom(n=length(x), size=1, prob=0.5))
+g <- factor(rbinom(n=length(x), size=1, prob=0.5)) # (@\wingding{1}@)
 y <- 3 * (x > 2 * pi) * x + 4 * sin(3 * x) + 10 * (as.numeric(g) - 1) +
-    rnorm(length(x), mean=0, sd=3)
-yL <- y > 25
-df1 <- data.frame(x, g, y, yL)
+    rnorm(length(x), mean=0, sd=3) # (@\wingding{2}@)
+yL <- y > 25 # (@\wingding{3}@)
+df1 <- data.frame(x, g, y, yL) # (@\wingding{4}@)
 head(df1, 4)
 summary(df1)
