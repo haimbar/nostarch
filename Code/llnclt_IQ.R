55268bc9c6b87da558c5750d3b3e31fff95cdd29
Haim Bar <haim.bar@uconn.edu>
2023-05-24 21:55:57 -0400

    today's edit - chapter 4

--- Code/llnclt_IQ.R
+++ Code/llnclt_IQ.R
@@ -1,12 +1,14 @@
 set.seed(95473)
 n <- 200
-samp <- rnorm(n, 100, 15)
-cat("Mean=",mean(samp), ", SD=",sd(samp),"\n")
+samp1 <- rnorm(n, 100, 15)
+cat("Mean=",mean(samp1), ", SD=",sd(samp1),"\n")
 
-  
-qqnorm(samp, cex=0.7, pch=18, col="purple")
-abline(100,15,col="orange", lwd=3)
+pdf("images/chapter_4/qqplot1.pdf")
+qqnorm(samp, cex=0.9, pch=18, col="purple")
+abline(100, 15, col="orange", lwd=3)
+dev.off()
 
+pdf("images/chapter_4/LLN_IQ.pdf")
 set.seed(95473)
 ns <- seq(10, 2000, by=10)
 L <- length(ns)
@@ -18,3 +20,4 @@ for (i in 1:L) {
 plot(ns, allMeans, pch=19, cex=0.5, col=3, axes=FALSE)
 axis(1); axis(2)
 abline(h=100, lwd=3,col=2)
+dev.off()
