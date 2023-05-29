e9f1927c3c36c4d52ad48c109ce71b3202c2aa08
Haim Bar <haim.bar@uconn.edu>
2023-05-29 18:18:23 -0400

    finished the LLN section

--- Code/llnclt_texts.R
+++ Code/llnclt_texts.R
@@ -1,10 +1,12 @@
-ssize <- c(5,seq(10,1000,by=10))
-myMsg <- rep(0,length(ssize))
+pdf("images/chapter_4/LLN_texts.pdf")
+ssize <- c(5, seq(10, 1000, by=10))
+myMsg <- rep(0, length(ssize))
 set.seed((40001))
 for (i in 1:length(ssize)) {
-    myMsg[i] <- mean(12*rpois(ssize[i], 10))
+    myMsg[i] <- mean(12*rpois(ssize[i], 10))  # (@\wingding{1}@)
 }
-plot(ssize, myMsg,pch=17,col="blue", axes=FALSE)
+plot(ssize, myMsg, pch=17, col="blue", axes=FALSE, xlab="Sample Size",
+     ylab="Average number of texts")
 axis(1); axis(2)
 abline(h=12*10, lwd=2,col=2)
-
+dev.off()
