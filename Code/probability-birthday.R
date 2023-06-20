1ce75ee150cede72972bdf7932605ce79dfebf52
HaiYing Wang <whygps@gmail.com>
2023-06-20 11:40:44 -0400

    update the birthday problem

--- Code/probability-birthday.R
+++ Code/probability-birthday.R
@@ -8,7 +8,7 @@ birthday <- function(n=23, yours="1980-05-01"){
     ## convert your birthday to date of year
     doy <- as.numeric(strftime(yours, format="%j"))
     ## if someone's birthday is the same as yours
-    same <- doy %in% bus
+    same <- doy %in% bus[-1]
     ## if there are people sharing a common birthday
     share <- length(unique(bus)) < n
     return (c(same, share))
@@ -19,15 +19,3 @@ prob.p23 <- rowMeans(res)
 ## plane with 253 people
 res <- replicate(n=1000, birthday(253))
 prob.p253 <- rowMeans(res)
-
-birthday <- function(n=23, yours="1980-05-01"){
-    N <- 10^6
-    bus <- sample(1:N, size=n, replace=TRUE) # randomly choose n people
-    doy <- as.numeric(strftime(yours, format="%j"))
-    same <- doy %in% bus
-    share <- length(unique(bus)) < n
-    return (c(same, share))
-}
-
-res <- replicate(n=1000, birthday(1000))
-rowMeans(res)
