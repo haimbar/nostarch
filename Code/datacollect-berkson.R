cc82243272dc98511eae59cb6f115ff87ef8ac4c
HaiYing Wang <whygps@gmail.com>
2023-07-03 18:15:57 -0400

    add data collection chapter

--- Code/datacollect-berkson.R
+++ Code/datacollect-berkson.R
@@ -1,17 +1,18 @@
 set.seed(2021)
 pdf("images/chapter_6/berkson1.pdf", width=4, height=4)
-n  <- 1000
-book  <- rnorm(n)
-movie <- rnorm(n)
-plot(book, movie) # plot all the books and movies
-abline(lm(movie~book))
+n  <- 1000              # total number of books and movies
+book  <- rnorm(n)       # all the n books
+movie <- rnorm(n)       # all the n movies
+plot(book, movie)       # plot all the books and movies
+abline(lm(movie~book))  # relation between all books and movies
 dev.off()
 
 pdf("images/chapter_6/berkson2.pdf", width=4, height=4)
-good.b <- book > quantile(book, 0.9)
-good.m <- movie > quantile(movie, 0.9)
-good  <- good.b | good.m
+good.b <- book > quantile(book, 0.9)   # indicator of good books
+good.m <- movie > quantile(movie, 0.9) # indicator of good movies
+good  <- good.b | good.m      # indicator of good books or movies
 plot(book[good], movie[good]) # plot only the good books or good movies
+## relation between good books or good movies
 abline(lm(movie[good]~book[good]))
 dev.off()
 
