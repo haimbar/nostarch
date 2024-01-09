1c66c1c6ea1344b72d92e3cc9f0e59958620abba
HaiYing Wang <whygps@gmail.com>
2024-01-09 12:37:06 -0500

    Finishing correlation and linear regression example

--- Code/regression-fitbodyfat.R
+++ Code/regression-fitbodyfat.R
@@ -1,13 +1,13 @@
 bodyfat <- read.csv("Data/bodyfat.csv")
-names(bodyfat)
-
 fit <- lm(BodyFat ~ Age + Weight + Chest + Wrist + BMI, data=bodyfat)
-summary(fit)
-coef(fit)
+print(fit)
+
+## names(bodyfat)
+## summary(fit)
+## coef(fit)
 
 pdf("images/chapter_regression/fitbodyfat.pdf", width=6, height=4)
 plot(bodyfat$BodyFat, fit$fitted.values, xlab="Body Fat",
      ylab="Estimated Body Fat")
+abline(1, 1)
 dev.off()
-
-print(fit)
