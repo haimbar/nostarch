d346a8ba266d2cefd0ce3542534509b5427147b1
HaiYing Wang <whygps@gmail.com>
2024-02-07 10:28:10 -0500

    update chapter 3 figure ratio

--- Code/probability-Simpson.R
+++ Code/probability-Simpson.R
@@ -4,8 +4,8 @@ n <- 40                           # number of samples in each group
 z <- rep(1:g, each = n)           # grouping variable
 x <- runif(n * g, 0, 2) + z       # x variable that depends on z
 y <- 3 * z - x + rnorm(n * g)     # y variable that depends on x and z
-pdf("images/chapter_3/probability-Simpson-plots.pdf", height=5, width=5)
-plot(x, y, pch=19, col=4, cex=0.7) # plot the whole data
+pdf("images/chapter_3/probability-Simpson-plots.pdf", height=3.5, width=3.5*1.5)
+plot(x, y, pch=19, col=4, cex=0.9) # plot the whole data
 abline(lm(y ~ x), lwd=3, col="navyblue")   # show the overall linear trend
 plot(x, y, pch = z, col = z)       # plot the data with group labeled
 for (grp in 1:g) # fit a line for each group
