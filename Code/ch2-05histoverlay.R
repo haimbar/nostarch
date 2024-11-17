a7419b06045cdaffdf1dd45fc4607a816b8668a9
Haim Bar <haim.bar@uconn.edu>
2024-11-17 18:23:59 -0500

    use named files for inline code for robustness

--- Code/ch2-05histoverlay.R
+++ Code/ch2-05histoverlay.R
@@ -1,6 +1,4 @@
 pdf("images/chapter_2/histoverlay.pdf", width=4, height=4)
-hist(y[g==0], breaks=20, xlim=range(y), col="orange", border="white",
-     main="", xlab="y")
-hist(y[g==1], breaks=20, xlim=range(y), col="#00008090", border="grey66",
-     add=TRUE)
+hist(y[g==0], breaks=20, xlim=range(y), col="orange", border="white", main="", xlab="y")
+hist(y[g==1], breaks=20, xlim=range(y), col="#00008090", border="grey66", add=TRUE)
 dev.off()
