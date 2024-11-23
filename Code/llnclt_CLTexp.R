16d7519f04e0beb9b7e13851125ee5954f8511ff
Haim Bar <haim.bar@uconn.edu>
2024-11-23 10:31:03 -0500

    reviewed chapter 4, updated the local runcode.sty file

--- Code/llnclt_CLTexp.R
+++ Code/llnclt_CLTexp.R
@@ -1,9 +1,9 @@
 pdf("images/chapter_4/CLTexp.pdf", width=5, height=5)
 set.seed(230630)
 n <- 100
-samp <- rexp(n, 5.5)
-hist(samp, freq = F, breaks=20,main="", xlab="x", border="white")
-xs <- seq(0, max(samp), length=1000)
+expsamp1 <- rexp(n, 5.5)
+hist(expsamp1, freq = F, breaks=20,main="", xlab="x", border="white")
+xs <- seq(0, max(expsamp1), length=1000)
 lines(xs, dexp(xs,5.5), lwd=3, col=2) # show the true distribution
 dev.off()
 
@@ -12,8 +12,8 @@ pdf("images/chapter_4/CLTexp2.pdf", width=8, height=5)
 n <- 50
 allMeans <- rep(0, 10000)   # (@\wingding{1}@)
 for (i in 1:10000) {
-  samp <- rexp(n,5.5)
-  allMeans[i] <- mean(samp) # (@\wingding{2}@)
+  expsamp <- rexp(n,5.5)
+  allMeans[i] <- mean(expsamp) # (@\wingding{2}@)
 }
 par(mfrow=c(1,2))
 m <- min(allMeans)
