92d921042c9302b4f2620e54c8c1545bca45ab71
HaiYing Wang <whygps@gmail.com>
2024-12-04 14:54:08 -0500

    explain code in text for bias chapter

--- Code/datacollect-berkson.R
+++ Code/datacollect-berkson.R
@@ -1,22 +1,19 @@
 set.seed(2021)
 pdf("images/chapter_6/berkson1.pdf", width=4, height=4)
-n  <- 1000              # total number of books and movies
-book  <- rnorm(n)       # all the n books
-movie <- rnorm(n)       # all the n movies
-plot(book, movie)       # plot all the books and movies
-abline(lm(movie~book))  # relation between all books and movies
+n  <- 1000
+book  <- rnorm(n)                       # (@\wingding{1}@)
+movie <- rnorm(n)                       # (@\wingding{2}@)
+plot(book, movie)
+abline(lm(movie~book))
 dev.off()
 
 pdf("images/chapter_6/berkson2.pdf", width=4, height=4)
-good.b <- book > quantile(book, 0.9)   # indicator of good books
-good.m <- movie > quantile(movie, 0.9) # indicator of good movies
-good  <- good.b | good.m      # indicator of good books or movies
-plot(book[good], movie[good]) # plot only the good books or good movies
-## relation between good books or good movies
+good.b <- book > quantile(book, 0.9)    # (@\wingding{3}@)
+good.m <- movie > quantile(movie, 0.9)  # (@\wingding{4}@)
+good  <- good.b | good.m                # (@\wingding{5}@)
+plot(book[good], movie[good])
 abline(lm(movie[good]~book[good]))
 dev.off()
 
-cat("Correlation between all books and movies:",
-    cor(book, movie), "\n")
-cat("Correlation between good books and good movies:",
-    cor(book[good], movie[good]), "\n")
+cat("Correlation between all books and movies:", cor(book, movie), "\n")
+cat("Correlation between good books and good movies:", cor(book[good], movie[good]), "\n")
