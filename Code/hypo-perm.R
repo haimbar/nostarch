8a6bd798903dba4485234998b36634e35b1ef06e
Jun Yan <1536656+jun-yan@users.noreply.github.com>
2025-11-29 08:38:24 -0500

    fixed labels in permutation example

--- Code/hypo-perm.R
+++ Code/hypo-perm.R
@@ -1,11 +1,9 @@
-### setting up data
 #label===HypoPermSetup
 set.seed(20210628)
 delta <- 0
 n1 <- n2 <- n <- 30
 x1 <- rnorm(n1)
 x2 <- rnorm(n2) + delta
-## a reasonable test statistic for a permutation test
 xd <- mean(x2) - mean(x1)               
 #===end
 
@@ -17,6 +15,7 @@ xdp <-  mean(xperm[n1 + 1:n2]) - mean(xperm[1:n1])
 
 ## How many possible permutations are there?
 ## If small, we can do exact test (like for the lady tasting tea)
+
 #label===HypoPermFullCount
 choose(n1 + n2, n1)
 #===end
