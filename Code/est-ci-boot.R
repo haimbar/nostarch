f4d0639a35d841eba11bf408a43481167ee32573
Jun Yan <1536656+jun-yan@users.noreply.github.com>
2026-06-06 08:16:18 -0400

    added figures to chapter 6

--- Code/est-ci-boot.R
+++ Code/est-ci-boot.R
@@ -38,3 +38,41 @@ mean(sim50[2,] < target & target < sim50[3,])
 ## let's try a smaller sample size
 sim20 <- replicate(nrep, do1rep(20))
 mean(sim20[2, ] < target & sim20[3,] > target)
+
+
+## Figure: show 50 bootstrap confidence intervals for two sample sizes
+make_boot_show <- function(n) {
+    boot_show <- t(replicate(50, do1rep(n)))
+    covered <- boot_show[, 2] < target & target < boot_show[, 3]
+    ord <- order(boot_show[, 1])
+    list(ci = boot_show[ord, 2:3], covered = covered[ord])
+}
+
+plot_boot_show <- function(obj, main, xlim) {
+    ci_show <- obj$ci
+    covered <- obj$covered
+    plot(NA, xlim = xlim, ylim = c(1, nrow(ci_show)),
+         xlab = "", ylab = "simulated interval", main = main)
+    abline(v = target, lwd = 2, lty = 2)
+    for (i in seq_len(nrow(ci_show))) {
+        col <- if (covered[i]) "gray45" else "firebrick3"
+        segments(ci_show[i, 1], i, ci_show[i, 2], i,
+                 lwd = 2, col = col)
+    }
+}
+
+set.seed(2028)
+boot_show50 <- make_boot_show(50)
+boot_show20 <- make_boot_show(20)
+xlim <- range(boot_show50$ci, boot_show20$ci, target)
+
+pdf("images/chapter_6/bootstrap_intervals_by_sample_size.pdf",
+    width = 7, height = 6)
+par(mfrow = c(2, 1), mar = c(3, 4, 2, 1))
+plot_boot_show(boot_show50, "n = 50", xlim)
+legend("bottomright",
+       legend = c("covered truth", "missed truth", "true median"),
+       col = c("gray45", "firebrick3", "black"),
+       lty = c(1, 1, 2), lwd = c(2, 2, 2), bty = "n")
+plot_boot_show(boot_show20, "n = 20", xlim)
+dev.off()
--- Code/est-hide-norm.R
+++ Code/est-hide-norm.R
@@ -1,10 +1,16 @@
-do1rep <- function(n, mu) {
+do1est <- function(n, mu) {
     x <- rnorm(n, mean = mu, sd = 1)              # generate data
     est <-  c(mu1 = mean(x), mu2 = median(x), mu3 = mean(range(x)))
+    return(est)                                   # return estimates
+}
+
+do1rep <- function(n, mu) {
+    est <- do1est(n, mu)
     return((est - mu)^2)                          # return squared error
 }
 
 
+do1est(50, mu)
 do1rep(50, mu)
 
 
@@ -14,5 +20,47 @@ sim <- replicate(nrep, do1rep(n, mu))         # run the race
 rowMeans(sim)                                 # summarize the means
 
 
+## Figure: compare estimates from several hide-and-seek games
+do1est_game <- function(n, mu, datagen) {
+    x <- datagen(n, mu)
+    c("Mean" = mean(x),
+      "Median" = median(x),
+      "Midrange" = mean(range(x)))
+}
 
+make_estimates <- function(datagen) {
+    estimates <- t(replicate(nrep, do1est_game(n, mu, datagen)))
+    estimates - mu
+}
 
+plot_estimates <- function(est_sim, main) {
+    cols <- c("skyblue3", "seagreen3", "salmon")
+    ylim <- quantile(est_sim, c(0.02, 0.98))
+    plot(NA, xlim = c(0.5, 3.5), ylim = ylim, xaxt = "n",
+         xlab = "", ylab = "estimate - truth", main = main)
+    abline(h = 0, lwd = 2, lty = 2)
+    for (j in seq_len(ncol(est_sim))) {
+        vals <- est_sim[, j]
+        vals <- vals[ylim[1] <= vals & vals <= ylim[2]]
+        d <- density(vals)
+        d$y <- d$y / max(d$y) * 0.35
+        polygon(c(j - d$y, rev(j + d$y)),
+                c(d$x, rev(d$x)),
+                col = cols[j], border = "gray40")
+        points(rep(j, 80), sample(vals, 80),
+               pch = 16, cex = 0.22, col = rgb(0, 0, 0, 0.25))
+    }
+    axis(1, at = 1:3, labels = colnames(est_sim))
+}
+
+set.seed(2026)
+est_norm <- make_estimates(function(n, mu) rnorm(n, mean = mu, sd = 1))
+est_cauchy <- make_estimates(function(n, mu) mu + rt(n, df = 1))
+est_unif <- make_estimates(function(n, mu) runif(n, mu - 10, mu + 10))
+
+pdf("images/chapter_6/estimator_violin_by_game.pdf", width = 7, height = 6)
+par(mfrow = c(3, 1), mar = c(2.4, 4, 2, 1))
+plot_estimates(est_norm, "Normal data")
+plot_estimates(est_cauchy, "Cauchy data")
+plot_estimates(est_unif, "Uniform data")
+dev.off()
