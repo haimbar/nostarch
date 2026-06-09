63dd338c604cd5d24ba1ba5431eed388aa9adaf9
HaiYing Wang <whygps@gmail.com>
2026-06-09 10:42:56 -0400

    install R packages in personal library when initializing the book

--- Code/bookinit.R
+++ Code/bookinit.R
@@ -1,12 +1,21 @@
+## 1. Dynamically find or create the default personal library path
+user_lib <- Sys.getenv("R_LIBS_USER")
+
+## 2. Create the directory if it doesn't exist yet
+if (!dir.exists(user_lib)) {
+  dir.create(user_lib, recursive = TRUE, showWarnings = FALSE)
+}
+
+## 3. Install the package into that personal library
 if (! ("lattice" %in% installed.packages()))
-	install.packages("lattice", repos = "https://cloud.r-project.org")
+	install.packages("lattice", repos = "https://cloud.r-project.org", lib = user_lib)
 library("lattice")
 if (! ("xtable" %in% installed.packages()))
-	install.packages("xtable", repos = "https://cloud.r-project.org")
+	install.packages("xtable", repos = "https://cloud.r-project.org", lib = user_lib)
 library("xtable")
 if (! ("pastecs" %in% installed.packages()))
-	install.packages("pastecs", repos = "https://cloud.r-project.org")
+	install.packages("pastecs", repos = "https://cloud.r-project.org", lib = user_lib)
 library("pastecs")
 if (! ("pracma" %in% installed.packages()))
-	install.packages("pracma", repos = "https://cloud.r-project.org")
+	install.packages("pracma", repos = "https://cloud.r-project.org", lib = user_lib)
 library("pracma")
